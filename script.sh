31d5c501b6643ec1ea8dbc8e33f02b03594198da
Onkar Anand <onkar406@aol.com>
2022-08-04 23:34:10 +0530

    Updated script.sh - 2
    Replaced expressions in sed with positional variables in script.

--- script.sh
+++ script.sh
@@ -7,4 +7,4 @@ sed -e '/<!-- Unit Tests Start -->/,/<!-- Unit Tests End -->/{/^<!--.*-->/!d}' \
       -e '/<!-- Unit Tests Start -->.*$/r unit-tests.txt' \
       -e '/<!-- Code Coverage Start -->/,/<!-- Code Coverage End -->/{/^<!--.*-->/!d}' \
       -e '/<!-- Code Coverage Start -->.*$/r ./coverage.txt' \
-       ./README.md | sed --expression='s/BRANCH/\$github_refname/g' | sed --expression='s/SHA/\$github_sha/g' >README.md.bak
+       ./README.md | sed --expression="s/BRANCH/\$github_refname/g" | sed --expression="s/SHA/\$github_sha/g" >README.md.bak
