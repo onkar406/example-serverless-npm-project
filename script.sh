4b7dad127f15b860630239f748156969b30c9c6d
Onkar Anand <onkar406@aol.com>
2022-08-08 16:51:46 +0530

    Updated script to the latest working version.

--- script.sh
+++ script.sh
@@ -16,7 +16,9 @@ github_sha="$2"
 #       ./README.md >./README.md.bak && sed -i "s/for.*$/${github_refname/\//\\/}: ${github_sha:0:7}/g" ./README.md.bak
 
 sed -e '/<!-- Unit Tests Start -->/,/<!-- Unit Tests End -->/{/^<!--.*-->/!d}' \
-      -e '/<!-- Unit Tests Start -->.*$/r unit-tests.txt' \
-      -e '/<!-- Code Coverage Start -->/,/<!-- Code Coverage End -->/{/^<!--.*-->/!d}' \
-      -e '/<!-- Code Coverage Start -->.*$/r ./coverage.txt' \
-      ./README.md >./README.md.bak && sed -i -e 's/^Branch:*/Branch: '"${github_refname/\//\\/}"'/g' README.md.bak && sed -i -e 's/Commit SHA: #/Commit SHA: #'"${github_sha:0:6}"'/g' README.md.bak
+    -e '/<!-- Unit Tests Start -->.*$/r unit-tests.txt' \
+    -e '/<!-- Code Coverage Start -->/,/<!-- Code Coverage End -->/{/^<!--.*-->/!d}' \
+    -e '/<!-- Code Coverage Start -->.*$/r ./coverage.txt' \
+    ./README.md >./README.md.bak \
+&& sed -i -e 's/^Branch:*,/Branch: '"${github_refname/\//\\/}"',/g' README.md.bak \
+&& sed -i -e 's/Commit SHA: #.*$/Commit SHA: #'"${github_sha:0:7}"'/g' README.md.bak
